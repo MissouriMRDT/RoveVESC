b2511bc363f11b9e5ba650d803ba10a28adc195b
Liam-Helfrich <whn7g@mst.edu>
2021-11-05 18:50:54 -0500

    Added more serial outputs

--- examples/getVescValues/getVescValues.ino
+++ examples/getVescValues/getVescValues.ino
@@ -12,6 +12,11 @@
 
 /** Initiate VescUart class */
 VescUart UART;
+VescUart UART3;
+VescUart UART4;
+VescUart UART5;
+VescUart UART6;
+VescUart UART7;
 
 /** Initiate RoveComm class */
 RoveCommEthernet RoveComm;
@@ -22,7 +27,7 @@ rovecomm_packet packet;
 float desiredRPM = 5000; /** The current in amps */
 
 //declare the Ethernet Server in the top level sketch with the requisite port ID any time you want to use RoveComm
-EthernetServer TCPServer(RC_ROVECOMM_ETHERNET_DRIVE_LIGHTING_BOARD_PORT);
+EthernetServer TCPServer(RC_ROVECOMM_DRIVEBOARD_PORT );
 
 void setup() {
 
@@ -31,11 +36,21 @@ void setup() {
 
   /** Setup UART port (Serial2 on Atmega32u4) */
   Serial2.begin(115200);
+  Serial3.begin(115200);
+  Serial4.begin(115200);
+  Serial5.begin(115200);
+  Serial6.begin(115200);
+  Serial7.begin(115200);
   
-  while (!Serial2) {;}
+  while (!(Serial2 && Serial3 && Serial4 && Serial5 && Serial6 && Serial7)) {;}
 
   /** Define which ports to use as UART */
   UART.setSerialPort(&Serial2);
+  UART3.setSerialPort(&Serial3);
+  UART4.setSerialPort(&Serial4);
+  UART5.setSerialPort(&Serial5);
+  UART6.setSerialPort(&Serial6);
+  UART7.setSerialPort(&Serial7);
 
 
   // Set up RoveComm with testing IP
@@ -66,12 +81,17 @@ void loop() {
   
   switch(packet.data_id)
   {
-    case RC_DRIVEBOARD_DRIVELEFTRIGHT_DATAID:
+    case RC_DRIVEBOARD_DRIVELEFTRIGHT_DATA_ID:
       //cast the packet to the correct data type
       int16_t* speeds;
       speeds = (int16_t*)packet.data;
       Serial.println(speeds[0]);
       UART.setRPM((float)speeds[0]); 
+      UART3.setRPM((float)speeds[0]); 
+      UART4.setRPM((float)speeds[0]); 
+      UART5.setRPM((float)speeds[0]); 
+      UART6.setRPM((float)speeds[0]); 
+      UART7.setRPM((float)speeds[0]); 
       break;
   }
   delay(50);
